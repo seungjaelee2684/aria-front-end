aecfac0892506787e6358efb10cec897763231b5
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-10-11 02:37:55 +0900

    <feat: SNS버튼 구현>

--- src/components/common/SNSMenu.tsx
+++ src/components/common/SNSMenu.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components';
-import Menu from '../../assets/icons/downArrow.png';
+import Instagram from '../../assets/icons/insta.png';
+import Twitter from '../../assets/icons/twitter.png';
+import Discord from '../../assets/icons/discord.png';
 
 const SNSMenu = () => {
+  
   return (
     <MenuIconContainer>
-      <MenuIcon src={Menu}/>
+      <InstaIcon>
+        <MenuIcon src={Instagram}/>
+      </InstaIcon>
+      <TwitterIcon>
+        <MenuIcon src={Twitter}/>
+      </TwitterIcon>
+      <DiscordIcon>
+        <MenuIcon src={Discord}/>
+      </DiscordIcon>
     </MenuIconContainer>
   )
 };
@@ -15,13 +26,62 @@ const MenuIconContainer = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  gap: 10px;
+`;
+
+const InstaIcon = styled.div`
+  width: 36px;
+  height: 36px;
+  display: grid;
+  justify-content: center;
+  place-items: inherit;
+  border: 1.5px solid #e54a5830;
+  border-radius: 100%;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    border: 1.5px solid #e54a58;
+  }
+`;
+
+const TwitterIcon = styled.div`
+  width: 36px;
+  height: 36px;
+  display: grid;
+  justify-content: center;
+  place-items: inherit;
+  border: 1.5px solid #33b2f930;
+  border-radius: 100%;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    border: 1.5px solid #33b2f9;
+  }
+`;
+
+const DiscordIcon = styled.div`
+  width: 36px;
+  height: 36px;
+  display: grid;
+  justify-content: center;
+  place-items: inherit;
+  border: 1.5px solid #7489da30;
+  border-radius: 100%;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    border: 1.5px solid #7489da;
+  }
 `;
 
 const MenuIcon = styled.img`
-  width: 36px;
-  height: 36px;
+  width: 24px;
+  height: 24px;
   object-fit: contain;
-  opacity: 0.5;
+  opacity: 0.3;
   transition: all 0.3s ease-in-out;
   cursor: pointer;
 
