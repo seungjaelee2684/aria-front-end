4162e34e04dc6639782208bfdb8c71c1502dd6a1
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-11-03 00:47:31 +0900

    <fix: footer css수정>

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Footer = () => {
+
+    const location = useLocation();
+    
   return (
-    <FooterContainer>
+    <FooterContainer style={{display: `${(location.pathname === "/") ? "none" : "block"}`}}>
         Footer
     </FooterContainer>
   )
@@ -12,14 +16,11 @@ const Footer = () => {
 const FooterContainer = styled.div`
     width: 100%;
     height: 200px;
-    background-color: #ADADAD;
+    background-color: #222020;
     color: #FCFCFC;
     font-family: "Pretendard";
-    /* position: absolute;
-    bottom: 0;
-    left: 0; */
     z-index: 99;
-    margin-top: 200px;
+    margin-top: 400px;
 
     @media screen and (max-width: 500px) {
         margin-bottom: 60px;
--- src/components/common/ScrollTop/ScrollTopButton.tsx
+++ src/components/common/ScrollTop/ScrollTopButton.tsx
@@ -54,7 +54,7 @@ const ScrollTopButtonWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #FFF;
+    background-color: rgba(255, 255, 255, 0.788);
     border: 2px solid #535151;
     width: 50px;
     height: 50px;
