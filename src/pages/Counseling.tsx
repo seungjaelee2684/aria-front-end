4f2655012646cf45d107efe7529abf3e39d4a98d
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-11-12 15:31:20 +0900

    <feat: 상담안내페이지 레버버튼 구현>

--- src/pages/Counseling.tsx
+++ src/pages/Counseling.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components';
 import Banner from '../components/common/Banner';
 
 const Counseling = () => {
 
   const language = localStorage.getItem("language");
+  const leverRef = useRef<HTMLDivElement>(null);
   const [isChoice, setIsChoice] = useState<boolean>(false);
+
+  const leversHandler = () => {
+    if (leverRef.current) {
+      if (isChoice) {
+        leverRef.current.style.transform = "translateX(0px)";
+      } else {
+        leverRef.current.style.transform = "translateX(-86px)";
+      };
+    };
+  };
 
   return (
     <LayOutContainer>
       {/* <Banner page={4}/> */}
       <ContentContainer>
         <ButtonWrapper>
-            {isChoice
+          <ButtonBackground
+            onClick={() => {
+              leversHandler()
+              setIsChoice(!isChoice)
+            }}>
+              Counseling
+            <LeversButton ref={leverRef}/>
+          </ButtonBackground>
+            {/* {isChoice
               ? <ButtonBox>
                 <Button onClick={() => setIsChoice(false)}>
                   Counseling
@@ -28,7 +47,7 @@ const Counseling = () => {
                 <Button onClick={() => setIsChoice(true)}>
                   Counseling
                 </Button>
-              </ButtonBox>}
+              </ButtonBox>} */}
         </ButtonWrapper>
       </ContentContainer>
     </LayOutContainer>
@@ -37,12 +56,12 @@ const Counseling = () => {
 
 const LayOutContainer = styled.div`
   width: 100%;
-  margin: 180px auto;
+  margin: 80px auto;
 `;
 
 const ContentContainer = styled.div`
   width: 1320px;
-  margin: 0px auto;
+  margin: 120px auto;
   display: flex;
   flex-direction: column;
 `;
@@ -52,6 +71,9 @@ const ButtonWrapper = styled.div`
   border-bottom: 2px solid #f3f3f3;
   position: relative;
   user-select: none;
+  display: flex;
+  justify-content: end;
+  align-items: center;
 `;
 
 const ButtonBox = styled.div`
@@ -100,6 +122,37 @@ const ClickButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const ButtonBackground = styled.div`
+  width: 140px;
+  height: 18px;
+  border-radius: 20px;
+  background-color: #e9e9e9;
+  position: relative;
+  box-shadow: inset 2px 2px 3px rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  color: #222020;
+  font-family: "Pretendard";
+  font-size: 12px;
+  font-weight: 500;
+  line-height: normal;
+  padding: 0px 10px;
+`;
+
+const LeversButton = styled.div`
+  width: 80px;
+  height: 22px;
+  border-radius: 20px;
+  background-color: #ADADAD;
+  position: absolute;
+  top: -2px;
+  left: 82px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.596);
+  transition: all 0.6s ease-in-out;
+  cursor: pointer;
 `;
 
 export default Counseling;
