53b6f1ddfc3b2b82d2bde4c44c44edec9c5a174b
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-11-23 23:57:08 +0900

    <feat: alert창 디자인 구현>

--- src/components/common/AlertModal/AlertModal.tsx
+++ src/components/common/AlertModal/AlertModal.tsx
@@ -4,8 +4,32 @@ import './AlertModal.css';
 
 const AlertModal = () => {
   return (
-    <div>AlertModal</div>
+    <BackgroudContainer>
+      <ModalContainer>
+        AlertModal
+      </ModalContainer>
+    </BackgroudContainer>
   )
 };
+
+const BackgroudContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 97;
+  background-color: #00000080;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainer = styled.div`
+  width: 400px;
+  height: 350px;
+  background-color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default AlertModal;
