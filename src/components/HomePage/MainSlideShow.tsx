b044c4b2d585fc34b5a12bf8edb67371f406921e
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-12-09 23:54:10 +0900

    <feat: 메인 페이지 슬라이드쇼 버튼 구현>

--- src/components/HomePage/MainSlideShow.tsx
+++ src/components/HomePage/MainSlideShow.tsx
@@ -18,14 +18,38 @@ const MainSlideShow = () => {
         if (mainSlideCurrent === 0) {
             setMainSlideCurrent(MainBannertData?.length - 1);
         };
+        setMainSlideCurrent(mainSlideCurrent - 1);
+    };
+
+    const onClickMainNextHandler = () => {
+        if (mainSlideCurrent === MainBannertData?.length - 1) {
+            setMainSlideCurrent(0);
+        };
         setMainSlideCurrent(mainSlideCurrent + 1);
     };
 
   return (
     <SlideShowOutContainer>
-        <SlideImage src={SlideImg}>
-            MainSlideShow
-        </SlideImage>
+        <SlideWrapper ref={mainSlideDivRef}>
+            {MainBannertData?.map((item) => {
+                return (
+                    <SlideImage key={item?.id} src={item?.image}>
+                        MainSlideShow
+                    </SlideImage>
+                )
+            })}
+        </SlideWrapper>
+        <NextPrevButtonWrapper>
+            {MainBannertData?.map((item) => {
+                return (
+                    <SlideButton key={item?.id}>
+                        {MainBannertData?.indexOf(item) + 1}
+                    </SlideButton>
+                )
+            })}
+            <SlideButton>P</SlideButton>
+            <SlideButton>N</SlideButton>
+        </NextPrevButtonWrapper>
     </SlideShowOutContainer>
   )
 };
@@ -35,20 +59,46 @@ const SlideShowOutContainer = styled.div`
     height: 600px;
     position: relative;
     background-color: #e9e9e9;
+    overflow: hidden;
 `;
 
 const SlideWrapper = styled.div`
     height: 100%;
+    display: flex;
+    align-items: center;
     transition: all 0.3 ease-out;
 `;
 
 const SlideImage = styled.div<{ src : string }>`
-    width: 100%;
+    min-width: 100%;
     height: 100%;
     background-image: url(${(props) => props.src});
     background-size: cover;
     background-position: 100% 100%;
     background-repeat: no-repeat;
+`;
+
+const NextPrevButtonWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    position: absolute;
+    bottom: 10%;
+    left: 10%;
+`;
+
+const SlideButton = styled.div`
+    width: 32px;
+    height: 32px;
+    border: 1px solid yellow;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Pretendard";
+    font-size: 18px;
+    font-weight: 600;
+    line-height: normal;
+    color: #222020;
 `;
 
 export default MainSlideShow;
