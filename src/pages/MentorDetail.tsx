79fc032044c98b63a206ad345b76d48a28b0e9e0
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-12-12 21:50:05 +0900

    <fix: 커리큘럼이미지 수정>

--- src/pages/MentorDetail.tsx
+++ src/pages/MentorDetail.tsx
@@ -69,15 +69,15 @@ const InContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 100px;
+    gap: 80px;
 
     @media screen and (max-width: 1100px) {
         width: 100%;
-        gap: 80px;
+        gap: 60px;
     }
 
     @media screen and (max-width: 500px) {
-        gap: 50px;
+        gap: 30px;
     }
 `;
 
