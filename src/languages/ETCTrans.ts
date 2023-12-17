4a58935c83fa95183ec0907c289f2383bfc59275
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2023-12-17 09:35:04 +0900

    <fix: 검색창 번역 추가>

--- src/languages/ETCTrans.ts
+++ src/languages/ETCTrans.ts
@@ -13,9 +13,15 @@ export const etcTextTrans : ETCTextTransType[] = [
         text: " 건"
     },
     {
-        englishtext: "Please enter your search term.",
+        englishtext: "Please enter here",
         chinesetext: "请输入搜索词。",
         japanesetext: "検索ワードを入力してください。",
         text: "검색어를 입력해주세요."
+    },
+    {
+        englishtext: "name",
+        chinesetext: "名字",
+        japanesetext: "名前",
+        text: "이름"
     },
 ];
