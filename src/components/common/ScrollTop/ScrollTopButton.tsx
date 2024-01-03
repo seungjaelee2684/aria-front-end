d31d8747d7aaee2ed0859a5f4efc122a56330694
DESKTOP-MMMPRLS\user <sean2684@naver.com>
2024-01-03 11:23:13 +0900

    <update: 스크롤 top버튼 메인페이지 삭제처리>

--- src/components/common/ScrollTop/ScrollTopButton.tsx
+++ src/components/common/ScrollTop/ScrollTopButton.tsx
@@ -1,28 +1,46 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './ScrollTop.css';
 import styled from 'styled-components';
 import { LuArrowBigUpDash } from 'react-icons/lu';
 import { BiArrowToTop } from 'react-icons/bi';
+import { useLocation } from 'react-router-dom';
 
 const ScrollTopButton = () => {
 
-    const scrollBtn = useRef<HTMLDivElement>(null);
+    const location = useLocation();
 
-    window.addEventListener("scroll", () => {
-        let scrollValue = window.scrollY;
-        // console.log("스크롤", scrollValue);
-        if (scrollBtn.current) {
-            if (scrollValue > 300) {  
-                scrollBtn.current.style.animation = "scroll_btn_open 0.2s ease-out forwards";
-            } else {
-                if (scrollValue === 0) {
-                    scrollBtn.current.style.opacity = "0";
-                } else {
+    const scrollBtn = useRef<HTMLDivElement>(null);
+    const mainScrollBtn = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const scrollTopEvent = (e : any) => {
+            let scrollValue = window.scrollY;
+            // console.log("스크롤", scrollValue);
+            if (scrollBtn.current) {
+                if (location.pathname === "/") {
                     scrollBtn.current.style.animation = "scroll_btn_close 0.2s ease-out forwards";
-                };   
+                } else {
+                    if (scrollValue > 300) {  
+                        scrollBtn.current.style.animation = "scroll_btn_open 0.2s ease-out forwards";
+                    } else {
+                        if (scrollValue === 0) {
+                            scrollBtn.current.style.opacity = "0";
+                        } else {
+                            scrollBtn.current.style.animation = "scroll_btn_close 0.2s ease-out forwards";
+                        };   
+                    };
+                }; 
             };
         };
-    });
+
+        window.addEventListener("scroll", scrollTopEvent);
+
+        return () => {
+            window.removeEventListener("scroll", scrollTopEvent);
+        };
+    }, [location.pathname]);
+
+    
 
     const onClickMoveTopHandler = () => {
         window.scrollTo({
@@ -45,7 +63,7 @@ const ScrollTopButtonWrapper = styled.div`
     position: fixed;
     z-index: 99;
     bottom: 4%;
-    right: 4%;
+    right: 2%;
     font-family: "Pretendard";
     font-size: 24px;
     font-weight: 400;
