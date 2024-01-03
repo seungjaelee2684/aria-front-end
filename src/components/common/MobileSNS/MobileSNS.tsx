073dcbaa0470e33601a2e6993204637236029cf7
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-01-03 22:23:53 +0900

    <fix: 반응형 sns모달창 구현>

--- src/components/common/MobileSNS/MobileSNS.tsx
+++ src/components/common/MobileSNS/MobileSNS.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import '../../../style/font/font.css';
 import './MobileSNS.css';
 import Instagram from '../../../assets/icons/insta.webp';
 import Twitter from '../../../assets/icons/twitter.webp';
@@ -8,6 +9,11 @@ import Youtube from '../../../assets/icons/youtube.webp';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { translate } from '../../../store/Translation';
 import { AlertModalOpen } from '../../../store/AlertModalOpen';
+import { MdClose } from 'react-icons/md';
+import { BsInstagram, BsDiscord } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FiYoutube } from "react-icons/fi";
+import { MdOutlineSupervisorAccount } from "react-icons/md";
 
 interface MobileSNSProps {
     snsModal: boolean;
@@ -32,94 +38,159 @@ const MobileSNS : React.FC<MobileSNSProps> = ({ snsModal, setSnsModal }) => {
     };
 
   return (
-    <div>
-        <BackgroundContainer onClick={() => setSnsModal(false)}/>
-        <IconBox
-            bordercolor="1px solid #e54a58"
-            style={{bottom: '70px', left: "25%"}}
-            className='InstaIcon'
-            onClick={() => setAlertModal({...alertModal, isOpen: true, whatAlert: 0})}>
-            <Icon src={Instagram} alt=''/>
-        </IconBox>
-        <IconBox
-            bordercolor="1px solid #33b2f9"
-            style={{bottom: '120px', left: "36%"}}
-            className='TwitterIcon'
-            onClick={() => {
-                window.open("https://twitter.com/ARIA_Academy");
-                setSnsModal(false);
-            }}>
-            <Icon src={Twitter} alt=''/>
-        </IconBox>
-        <IconBox
-            bordercolor="1px solid #7489da"
-            style={{bottom: '120px', left: "52%"}}
-            className='DiscordIcon'
-            onClick={() => {
-                window.open("https://discord.gg/N7SEvBds4F")
-                setSnsModal(false);
-            }}>
-            <Icon src={Discord} alt=''/>
-        </IconBox>
-        <IconBox
-            bordercolor="1px solid #ff0000"
-            style={{bottom: '70px', left: "63%"}}
-            className='YoutubeIcon'
-            onClick={() => setAlertModal({...alertModal, isOpen: true, whatAlert: 0})}>
-            <Icon src={Youtube} alt=''/>
-        </IconBox>
-    </div>
+    <MobileSNSModalContainer className='mobile-SNS-modal'>
+        <CloseButton onClick={() => setSnsModal(false)}>
+            <MdClose />
+        </CloseButton>
+        <ModalTitleContainer>
+            <MdOutlineSupervisorAccount />
+            SNS
+        </ModalTitleContainer>
+        <IconWrapper>
+            <InstaIconBox
+                onClick={() => setAlertModal({...alertModal, isOpen: true, whatAlert: 0})}>
+                <Icon>
+                    <BsInstagram />
+                    <Text>
+                        Instagram
+                    </Text>
+                </Icon>
+            </InstaIconBox>
+            <TwitterIconBox
+                onClick={() => {
+                    window.open("https://twitter.com/ARIA_Academy");
+                    setSnsModal(false);
+                }}>
+                <Icon>
+                    <RiTwitterXFill />
+                    <Text>
+                        TwitterX
+                    </Text>
+                </Icon>
+            </TwitterIconBox>
+            <DiscordIconBox
+                onClick={() => {
+                    window.open("https://discord.gg/N7SEvBds4F")
+                    setSnsModal(false);
+                }}>
+                <Icon>
+                    <BsDiscord />
+                    <Text>
+                        Discord
+                    </Text>
+                </Icon>
+            </DiscordIconBox>
+            <YoutubeIconBox
+                onClick={() => setAlertModal({...alertModal, isOpen: true, whatAlert: 0})}>
+                <Icon>
+                    <FiYoutube />
+                    <Text>
+                        Youtube
+                    </Text>
+                </Icon>
+            </YoutubeIconBox>
+        </IconWrapper>
+    </MobileSNSModalContainer>
   )
 };
 
-const BackgroundContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 98;
-  background-color: #22202070;
-`;
+// const BackgroundContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   z-index: 98;
+//   background-color: #22202070;
+// `;
 
 const MobileSNSModalContainer = styled.div`
     width: 100%;
     height: 100%;
     position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 102;
+    background-color: #222020;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 60px;
 `;
 
-const IconBox = styled.div<{ bordercolor : string }>`
-    width: 50px;
+const ModalTitleContainer = styled.div`
+    font-family: "CinzelBlack";
+    font-size: 32px;
+    font-weight: 700;
+    line-height: normal;
+    color: #FFFFFF;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+`;
+
+const CloseButton = styled.div`
+    font-size: 32px;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: #FFFFFF;
+`;
+
+const IconWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+`;
+
+export const InstaIconBox = styled.div`
+    width: 80%;
     height: 50px;
     display: flex;
     justify-content: center;
     align-items: center;
-    border: ${(props) => props.bordercolor};
-    border-radius: 100%;
-    background-color: rgba(255, 255, 255, 0.925);
-    box-shadow: rgba(94, 92, 92, 0.808) 1px 1px 10px 1px;
-    position: fixed;
-    z-index: 98;
-    bottom: 70px;
-    left: 25%;
-    opacity: 0;
-
-    /* &::before {
-        content: '';
-        width: 100%;
-        height: 100%;
-        border: 1px solid;
-        border-image: linear-gradient(${(props) => props.bordercolor});
-        border-image-slice: 1;
-        border-radius: 50%;
-        position: absolute;
-    } */
+    color: #FFFFFF;
+    background: linear-gradient(to right, #f9cc01, #fa1f02, #b93382, #914bc4);
+    cursor: pointer;
+    /* box-shadow: rgba(94, 92, 92, 0.808) 1px 1px 10px 1px; */
 `;
 
-const Icon = styled.img`
-    width: 40px;
-    height: 40px;
-    object-fit: contain;
+const TwitterIconBox = styled(InstaIconBox)`
+    background: none;
+    background-color: #000000;
+    /* box-shadow: rgba(94, 92, 92, 0.808) 1px 1px 10px 1px; */
+`;
+
+const DiscordIconBox = styled(InstaIconBox)`
+    background: none;
+    background-color: #7489da;
+    /* box-shadow: rgba(94, 92, 92, 0.808) 1px 1px 10px 1px; */
+`;
+
+const YoutubeIconBox = styled(InstaIconBox)`
+    background: none;
+    background-color: #ff0000;
+    /* box-shadow: rgba(94, 92, 92, 0.808) 1px 1px 10px 1px; */
+`;
+
+const Icon = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 30px;
+    font-size: 26px;
+`;
+
+const Text = styled.div`
+    font-family: "CinzelBlack";
+    font-size: 22px;
+    font-weight: 400;
+    line-height: normal;
 `;
 
 export default MobileSNS;
