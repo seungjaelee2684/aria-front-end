22440f6ba3bf335539fbbf44236c19d30d8cc295
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-02-04 20:01:10 +0900

    <fix: 강사페이지 슬라이드배너 반응형 style수정>

--- src/components/MentorPage/SlideShow.tsx
+++ src/components/MentorPage/SlideShow.tsx
@@ -205,8 +205,8 @@ const SlideNumber = styled.div`
   min-width: 10px;
   height: 10px;
   border-radius: 100%;
-  background-color: #ADADAD;
-  box-shadow: 1px 1px 6px 0px #5a5a5a;
+  background-color: #4e4e4e;
+  box-shadow: 1px 1px 6px 0px #535353;
   transition: all 0.6s ease-in-out;
   cursor: pointer;
 `;
