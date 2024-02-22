44cfd5a12e2e310b241767aca58a0c0ef16149b0
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2024-02-22 16:43:53 +0900

    <feat: 공지사항 업로드 페이지 마크업>

--- src/pages/Mentor.tsx
+++ src/pages/Mentor.tsx
@@ -16,9 +16,11 @@ import { nationKind } from '../store/NationFilter';
 import { getMentorsApi } from '../api/mentors';
 import SlideWrapper from '../components/MentorPage/SlideWrapper';
 import { isLogin } from '../store/IsLogin';
+import { useNavigate } from 'react-router-dom';
 
 const Mentor = () => {
 
+  const navigate = useNavigate();
   const page = useRecoilValue(pageNumber);
   const nationFilter = useRecoilValue(nationKind);
   const loginState = useRecoilValue(isLogin);
@@ -41,6 +43,12 @@ const Mentor = () => {
         <TitleColorText color="#7769D0">R</TitleColorText>
         TIST
       </MentorPageTitleContainer>
+      {(data?.data.isOperator)
+        && <UploadButtonWrapper>
+            <UploadButton onClick={() => navigate("/upload/mentor")}>
+              강사 등록하기
+            </UploadButton>
+          </UploadButtonWrapper>}
       <MentorList data={data} loginState={loginState}/>
       <MentorSearchBar data={data}/>
     </LayoutContainer>
@@ -65,6 +73,36 @@ const MentorPageTitleContainer = styled(LayOutTitleContainer)`
 
   @media screen and (max-width: 500px) {
     margin-top: 40px;
+  }
+`;
+
+const UploadButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UploadButton = styled.button`
+  width: 120px;
+  height: 36px;
+  border: none;
+  outline: none;
+  border-radius: 3px;
+  font-family: "Pretendard";
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 100%;
+  color: #FFFFFF;
+  background-color: #5C9DFF;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #83b2f8;
+  }
+
+  &:active {
+    width: 118px;
   }
 `;
 
---
+++ src/pages/NoticeUpload.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const NoticeUpload = () => {
+  return (
+    <AllContainer>
+        NoticeUpload
+    </AllContainer>
+  )
+};
+
+const AllContainer = styled.div`
+  width: 1320px;
+  margin: 0px auto;
+  padding: 80px 0px 40px 0px;
+`;
+
+export default NoticeUpload;
